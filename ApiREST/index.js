4e910cbc5ea8fe6b0637df40fef9e7d2f5d902a8
Anahi <anahi.z.a@gmail.com>
2018-12-08 16:55:05 -0600

    Agregando rutas

--- ApiREST/index.js
+++ ApiREST/index.js
@@ -9,10 +9,26 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get("/hola/:name", (req, res) => {
-    res.send({message: `Hola ${req.params.name}`})
+app.get("/api/product", (req, res) => {
+    res.send(200, {products: []})
 })
 
+app.get("/api/product/:id", (req, res) => {
+
+})
+
+app.post("/api/product", (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: "El producto se ha recibido"})
+})
+
+app.put("/api/product/:id", (req, res) => {
+
+})
+
+app.delete("/api/procut/:id", (req, res)=> {
+
+})
 app.listen(port, ()=> {
     console.log(`Api REST funcionando en http://localhost:${port}`)
 })
