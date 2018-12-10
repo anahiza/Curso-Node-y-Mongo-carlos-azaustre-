1cbfae2405f8b76c0e3f4e949edcd8bcadc5cf93
Anahi <anahi.z.a@gmail.com>
2018-12-09 18:45:38 -0600

    Guardando productos en db

--- ApiREST/index.js
+++ ApiREST/index.js
@@ -2,10 +2,12 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
+const Product = require('./Models/Product')
 
 const app = express()
 const port = process.env.PORT || 3000
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
@@ -18,8 +20,23 @@ app.get("/api/product/:id", (req, res) => {
 })
 
 app.post("/api/product", (req, res) => {
+    console.log('POST/Product')
     console.log(req.body)
-    res.status(200).send({message: "El producto se ha recibido"})
+    let product = new Product()
+    product.name = req.body.name
+    product.picture = req.body.picture
+    product.price = req.body.price
+    product.category = req.body.category
+    product.description = req.body.description
+    
+    product.save((err, p) => {
+        if (err){
+            res.status(500).send(`Error al guardar en db ${err}`)
+        }
+        res.status(200).send({product: p})
+ 
+    })
+   
 })
 
 app.put("/api/product/:id", (req, res) => {
@@ -29,6 +46,12 @@ app.put("/api/product/:id", (req, res) => {
 app.delete("/api/procut/:id", (req, res)=> {
 
 })
-app.listen(port, ()=> {
+
+mongoose.connect("mongodb://localhost:27017/shop", (error, res) => {
+    if (error) 
+        return console.log(`Error al conectar a la db ${error}`)
+    console.log("Conexion a db establecida")
+    app.listen(port, ()=> {
     console.log(`Api REST funcionando en http://localhost:${port}`)
+})
 })
