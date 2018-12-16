18b5f7674defbca122897c6513aa6d4877f1b7e6
Anahi <anahi.z.a@gmail.com>
2018-12-16 15:10:33 -0600

    Implementando PUT para actualizar producto

--- ApiREST/index.js
+++ ApiREST/index.js
@@ -29,7 +29,6 @@ app.get("/api/product/:id", (req, res) => {
         if (!product) return res.status(404).send({message: `El producto no existe`})
         res.status(200).send({product})
     })
-
 })
 
 app.post("/api/product", (req, res) => {
@@ -53,19 +52,24 @@ app.post("/api/product", (req, res) => {
 })
 
 app.put("/api/product/:id", (req, res) => {
-
+    let productId = req.params.id  
+    let update = req.body    
+    Product.findByIdAndUpdate(productId, update, (err, productUpdated) => {
+        if (err) {
+            res.status(500).send({ message: `Error al actualizar el producto ${err}` })
+        }
+        console.log(productUpdated)
+        res.status(200).send({ product: productUpdated })
+    })
 })
 
 app.delete("/api/product/:id", (req, res)=> {
     let pID = req.params.id
-    console.log(pID)
     Product.findById(pID, (err, product) => {
-        if (err){
-            console.log(err)
-            res.status(500).send(`Error al borrar el producto ${productId} $err`)
+        if (err) {    
+            res.status(500).send(`Error al borrar el producto ${productId} ${err}`)
         }
-        console.log(product)
-        product.remove( err => {
+        product.remove((err) => {
             if (err){
                 console.log(err)
                 res.status(500).send(`Error al borrar el producto ${productId} ${err}`)
@@ -82,7 +86,7 @@ mongoose.connect("mongodb://localhost:27017/shop", (error, res) => {
         return console.log(`Error al conectar a la db ${error}`)
     }
     console.log("Conexion a db establecida")
-    app.listen(port, ()=> {
+    app.listen(port, () => {
         console.log(`Api REST funcionando en http://localhost:${port}`)
     })
 })
