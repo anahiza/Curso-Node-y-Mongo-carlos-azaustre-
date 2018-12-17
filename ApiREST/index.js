be00e9b216f6efa3d07dfd755451d6e1df38d704
Anahi <anahi.z.a@gmail.com>
2018-12-16 22:31:44 -0600

    Refactorizando código 2

--- ApiREST/index.js
+++ ApiREST/index.js
@@ -1,33 +1,17 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Product = require('./Models/Product')
-
-const app = express()
+const app = require('./app')
+const config = require('./config')
 const port = process.env.PORT || 3000
-const ProductController = require('./Controllers/product')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-app.get("/api/product", ProductController.getProducts)
-
-app.get("/api/product/:id", ProductController.getProduct)
-
-app.post("/api/product", ProductController.saveProduct)
-
-app.put("/api/product/:id", ProductController.updateProduct)
-
-app.delete("/api/product/:id", ProductController.deleteProduct)
-
-mongoose.connect("mongodb://localhost:27017/shop", (error, res) => {
+mongoose.connect(config.db, (error, res) => {
     if (error) {
         return console.log(`Error al conectar a la db ${error}`)
     }
     console.log("Conexion a db establecida")
-    app.listen(port, () => {
-        console.log(`Api REST funcionando en http://localhost:${port}`)
+    app.listen(config.port, () => {
+        console.log(`Api REST funcionando en ${config.url}:${config.port}`)
     })
 })
---
+++ ApiREST/routes/index.js
@@ -0,0 +1,12 @@
+'use strict'
+const express = require('express')
+const api = express.Router()
+const ProductController = require('../Controllers/product')
+
+api.get("/product", ProductController.getProducts)
+api.get("/product/:id", ProductController.getProduct)
+api.post("/product", ProductController.saveProduct)
+api.put("/product/:id", ProductController.updateProduct)
+api.delete("/product/:id", ProductController.deleteProduct)
+
+module.exports = api
